51fedbc8c618029abde33ae8662758a6732a58ee
Shuyuan Zhang <shzhang@wiley.com>
2025-06-22 00:43:07 -0400

    added DbImporter

--- db/create-schema.sql
+++ db/create-schema.sql
@@ -6,18 +6,18 @@
 CREATE TABLE books (
     book_id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    rating DECIMAL(2, 1),
-    description TEXT,
-    language VARCHAR(255),
-    isbn VARCHAR(255) UNIQUE,
-    book_format VARCHAR(255),
-    edition VARCHAR(255),
-    pages INTEGER,
-    publisher VARCHAR(255),
-    publish_date DATE,
-    first_publish_date DATE,
-    liked_percent DECIMAL(5, 2),
-    price DECIMAL(5, 2)
+    rating DECIMAL(2, 1) NOT NULL,
+    description TEXT NOT NULL,
+    language VARCHAR(255) NOT NULL,
+    isbn VARCHAR(255)  NOT NULL,
+    book_format VARCHAR(255) NOT NULL,
+    edition VARCHAR(255) NOT NULL,
+    pages INT NOT NULL,
+    publisher VARCHAR(255) NOT NULL,
+    publish_date DATE NOT NULL,
+    first_publish_date DATE NOT NULL,
+    liked_percent DECIMAL(5, 2) NOT NULL,
+    price DECIMAL(5, 2) NOT NULL
 );
 
 -- Create the authors table
@@ -28,8 +28,8 @@ CREATE TABLE authors (
 
 -- Create the books_authors table
 CREATE TABLE books_authors (
-    book_id INTEGER NOT NULL,
-    author_id INTEGER NOT NULL,
+    book_id INT NOT NULL,
+    author_id INT NOT NULL,
     PRIMARY KEY (book_id, author_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
